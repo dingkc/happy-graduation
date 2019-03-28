d57cb7ea6829627b0a72593bd1f49863a9712937
丁可成 <dingkc@asiaInfo.com>
2019-03-28 12:20:09 +0800

    fix：更改菜单

--- src/ant-design-pro/config/router.config.js
+++ src/ant-design-pro/config/router.config.js
@@ -26,23 +26,23 @@ export default [
         path: '/dashboard',
         name: 'dashboard',
         icon: 'dashboard',
-        routes: [
-          {
-            path: '/dashboard/analysis',
-            name: 'analysis',
-            component: './Dashboard/Analysis',
-          },
-          {
-            path: '/dashboard/monitor',
-            name: 'monitor',
-            component: './Dashboard/Monitor',
-          },
-          {
-            path: '/dashboard/workplace',
-            name: 'workplace',
-            component: './Dashboard/Workplace',
-          },
-        ],
+        // routes: [
+        //   {
+        //     path: '/dashboard/analysis',
+        //     name: 'analysis',
+        //     component: './Dashboard/Analysis',
+        //   },
+        //   {
+        //     path: '/dashboard/monitor',
+        //     name: 'monitor',
+        //     component: './Dashboard/Monitor',
+        //   },
+        //   {
+        //     path: '/dashboard/workplace',
+        //     name: 'workplace',
+        //     component: './Dashboard/Workplace',
+        //   },
+        // ],
       },
       // forms
       {
--- src/ant-design-pro/src/defaultSettings.js
+++ src/ant-design-pro/src/defaultSettings.js
@@ -1,18 +1,19 @@
 module.exports = {
-  navTheme: 'dark', // theme for nav menu
+  navTheme: 'light', // theme for nav menu
   primaryColor: '#1890FF', // primary color of ant design
-  layout: 'sidemenu', // nav menu position: sidemenu or topmenu
-  contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
-  fixedHeader: false, // sticky header
-  autoHideHeader: false, // auto hide header
+  layout: 'topmenu', // nav menu position: sidemenu or topmenu
+  contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
+  fixedHeader: true, // sticky header
+  autoHideHeader: true, // auto hide header
   fixSiderbar: false, // sticky siderbar
   menu: {
     disableLocal: false,
   },
-  title: 'Ant Design Pro',
+  title: '可云·云平台',
   pwa: true,
   // your iconfont Symbol Scrip Url
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
   // 注意：如果需要图标多色，Iconfont图标项目里要进行批量去色处理
   iconfontUrl: '',
+  collapse: true,
 };
